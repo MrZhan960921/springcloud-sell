cf1f374483061ad0516c2c056bd09897e6308912
zcq <zcq@zcqdeMacBook-Pro.local>
2019-09-10 01:01:42 +0800

    Spring Cloud Bus实现配置自动刷新

---
+++ order/order-server/src/main/java/com/zcq/controller/EnvController.java
@@ -0,0 +1,25 @@
+package com.zcq.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Maybeeeee
+ * @date 2019-09-09 00:06
+ */
+@RestController
+@RequestMapping("/env")
+@RefreshScope
+public class EnvController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String print() {
+        return env;
+    }
+}
