75adc1a262a9aab90730f9fe37b76f1e16a3b8c7
zcq <zcq@zcqdeMacBook-Pro.local>
2019-09-17 23:38:07 +0800

    zuul的pre和post过滤器使用

---
+++ api-gateway/src/main/java/com/zcq/filter/TokenFilter.java
@@ -0,0 +1,54 @@
+package com.zcq.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * Zuul的过滤器
+ * @author Maybeeeee
+ * @date 2019-09-17 23:19
+ */
+@Component
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER-1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤的逻辑代码
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        //这里从url参数里获取, 也可以从cookie, header里获取
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)) {
+            //如果是空的，则没有权限，拒绝访问
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        return null;
+    }
+}
